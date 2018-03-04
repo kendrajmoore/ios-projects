3f813141630c46351e2095d4219d5ccb32de9f8f
Kendra Moore <kendra.j.moore3443@gmail.com>
2018-03-04 13:18:30 -0500

    second app

---
+++ dicee/dicee/ViewController.swift
@@ -0,0 +1,51 @@
+//
+//  ViewController.swift
+//  dicee
+//
+//  Created by Kendra Moore on 3/4/18.
+//  Copyright © 2018 Kendra Moore. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    var randomDiceIndex1 : Int = 0
+    var randomDiceIndex2 : Int = 0
+    
+    let diceArray = ["dice1", "dice2", "dice3", "dice4", "dice5", "dice6"]
+    
+    @IBOutlet weak var diceImage1: UIImageView!
+    @IBOutlet weak var diceImage2: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        updateDiceImages()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func rollButtonPressed(_ sender: UIButton) {
+        
+        updateDiceImages()
+        
+    }
+    
+    func updateDiceImages(){
+        randomDiceIndex1 = Int(arc4random_uniform(6))
+        randomDiceIndex2 = Int(arc4random_uniform(6))
+        
+        diceImage1.image = UIImage(named: diceArray[randomDiceIndex1])
+        diceImage2.image = UIImage(named: diceArray[randomDiceIndex2])
+    }
+    override func motionEnded(_ motion: UIEventSubtype, with event: UIEvent?) {
+        updateDiceImages()
+    }
+    
+}
+
